acf0f340767ec46e8003a7b86b1d98ecbda1f000
Luis Sepulveda <daemo159@gmail.com>
2022-07-30 00:33:37 -0500

    Modified proto.uwu.php in attempt to make a logger lock

--- ProtoSUDO PHP/proto.uwu.php
+++ ProtoSUDO PHP/proto.uwu.php
@@ -4,49 +4,49 @@ use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Event;
 //require_once('./logger.php');
+$log = "1";
 require('./key.php');
 $key = getKey();
 $discord = new Discord(['token' => $key]);
 $discord->on('ready', function (Discord $discord) {
-    echo "Bot is ready!", PHP_EOL;
-    $log = false;
+    $log = 1;
+    echo "Bot is ready! echo LOG status: {$log};", PHP_EOL;
     $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord, $log) {
         //---------------------------------------- DATA LOGGER ------------------------------------------- //
+        echo "LOG status: {$log}";
         $user = "<@{$message->author->id}>";
         $guild = $message->guild->name;
         $content = $message->content;
         if($content === 'p!test'){
-            try {
-                //  
-                    
+            try{
                     $message->reply("Hey! {$user}, estoy vivo!");
                     echo "$user";
+                    echo "LOG status: {$log}";
                 //var_dump($message);
                 } catch (\Throwable $th) {
                     $message->reply("hubo un error: {$th}");
                     echo "$th";
+                    echo "LOG status: {$log}";
                 };
         };
         if($content === 'p!log enable'){
-            $log = true;
+            echo "status: {$log}";
+            $log = 1;
             $message->reply("se ha activado el log de mensajes. contacta con <@610937299903184898> para obtener el log cuando sea requerido.");
-            return($log);
         }
         if($content === 'p!log disable'){
-                $log = false;
+                echo "status: {$log}";
+                $log = 0;
                 $message->reply("Se ha desactivado el log de mensajes");
-                return($log);
             }
-        
-         
-       /* try {
+        try {
             $consolelog= "{$message->author->username}: {$message->content}     ".date("F j, Y, g:i a");
             echo $consolelog.PHP_EOL;
         } catch (\Throwable $th) {
             echo "error while logging: $th".PHP_EOL;
-        }; */
-        //if($log === true){
-            /*if (strpos($content,'p!')=== false) {
+        }; 
+        if($log === 1){
+            if (strpos($content,'p!')=== false) {
                 try {
                     $dataToLog = "a command was triggered at ".date("F j, Y, g:i a") + "    command: {$content} ";
                     echo $dataToLog.PHP_EOL;
@@ -56,18 +56,19 @@ $discord->on('ready', function (Discord $discord) {
                 } catch (\Throwable $th) {
                     echo "error while logging: $th".PHP_EOL;
                 };
-            //} else {  */
+            } else { 
                 try {
                     $dataToLog = "{$message->author->username}: {$message->content}     ".date("F j, Y, g:i a");
                     echo $dataToLog.PHP_EOL;
-                    $pathToFile = "log {$message->guild->name}-{$message->channel->name}";    
+                    $pathToFile = ".\logs\log {$message->guild->name}-{$message->channel->name}";    
                     $data = $dataToLog.PHP_EOL;
                     file_put_contents($pathToFile.' '.date("F j, Y").'.txt',$data, FILE_APPEND);
                 } catch (\Throwable $th) {
                     echo "error while logging: $th".PHP_EOL;
                 };
-           // }
-        //}
+            }
+        }
+        return($log);
     });
 });
 
